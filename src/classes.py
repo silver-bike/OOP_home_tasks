98aeaf3bd26f6ba8a1416dbc52528a10735fb914
silverbike <silverbike.python@gmail.com>
2024-06-17 23:43:54 +0400

    __str__

--- src/classes.py
+++ src/classes.py
@@ -27,20 +27,23 @@ class Category:
     def show_products_in_stock(self):
         lst = self.__product
         return [(f'{product.name}, {product.price} руб. Остаток: '
-                 f'{product.quality} шт.') for product in lst]
+                 f'{product.quantity} шт.') for product in lst]
 
 
 class Product:
     name: str
     description: str
     price: float
-    quality: int
+    quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = round(price, 2)
-        self.quality = quantity
+        self.quantity = quantity
+
+    def __str__(self):
+        return f'{self.name} - {self.description} price:{self.price}    '
 
     @classmethod
     def new_product(cls, name, description, price, quantity):
@@ -58,29 +61,30 @@ class Product:
             print('введена некорректная цена')
 
 
-# product_1 = Product('Iphone', 'an Apple phone', 599.50, 20)
-# product_2 = Product('Ipod', 'an Apple media player', 249.50, 10)
-# product_3 = Product('IMac', 'an Apple computer', 1999.50, 5)
-# product_4 = Product('IMac', 'an Apple planchet', 449.50, 15)
-# category_1 = Category("Apple devises", "Some devises from Apple",
-#                       [product_1, product_2, product_3, product_4])
-#
-# product_5 = Product('Phone', 'a phone', 99.50, 120)
-# product_6 = Product('Player', 'a media player', 49.50, 210)
-# product_7 = Product('PC', 'a computer', 1999.50, 5)
-# product_8 = Product('planchet', 'a planchet', 99.50, 150)
-# category_2 = Category("Devises", "Some devises from Apple",
-#                       [product_5, product_6, product_7, product_8])
-#
-# print(Category.category_num)
-# print(Category.product_num)
-#
-# [print(x) for x in category_2.show_products_in_stock]
-#
-# product_11 = Product.new_product('кубик рубика', 'головоломка', 9.50, 10)
-#
-# print(product_11.price_)
-#
-# product_11.price_ = 3.88
-#
-# print(product_11.price_)
+product_1 = Product('Iphone', 'an Apple phone', 599.50, 20)
+product_2 = Product('Ipod', 'an Apple media player', 249.50, 10)
+product_3 = Product('IMac', 'an Apple computer', 1999.50, 5)
+product_4 = Product('IMac', 'an Apple planchet', 449.50, 15)
+category_1 = Category("Apple devises", "Some devises from Apple",
+                      [product_1, product_2, product_3, product_4])
+
+product_5 = Product('Phone', 'a phone', 99.50, 120)
+product_6 = Product('Player', 'a media player', 49.50, 210)
+product_7 = Product('PC', 'a computer', 1999.50, 5)
+product_8 = Product('planchet', 'a planchet', 99.50, 150)
+category_2 = Category("Devises", "Some devises from Apple",
+                      [product_5, product_6, product_7, product_8])
+
+print(Category.category_num)
+print(Category.product_num)
+
+[print(x) for x in category_2.show_products_in_stock]
+
+product_11 = Product.new_product('кубик рубика', 'головоломка', 9.50, 10)
+
+print(product_11.price_)
+
+product_11.price_ = 3.88
+
+print(product_11.price_)
+print(product_11)
