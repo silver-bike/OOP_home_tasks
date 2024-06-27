53fe22a82d4f87f8bfa06259ecfce57964c331b0
silverbike <silverbike.python@gmail.com>
2024-06-27 17:55:56 +0400

    new classes: Smartphone(Product) and Lawn_grass(Product)

--- src/classes.py
+++ src/classes.py
@@ -42,9 +42,10 @@ class Product:
     price: float
     quality: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name, description, colour, price, quantity):
         self.name = name
         self.description = description
+        self.colour = colour
         self.price = round(price, 2)
         self.quantity = quantity
 
@@ -52,8 +53,8 @@ class Product:
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
     @classmethod
-    def new_product(cls, name, description, price, quantity):
-        return cls(name, description, price, quantity)
+    def new_product(cls, name, description, colour, price, quantity):
+        return cls(name, description, colour, price, quantity)
 
     @property
     def price_(self):
@@ -70,26 +71,41 @@ class Product:
         return self.price * self.quantity + other.price * other.quantity
 
 
+class Smartphone(Product):
+    def __init__(self, name, description, colour, price, quantity, productivity, model, memory):
+        super().__init__(name, description, colour, price, quantity)
+        self.productivity = productivity
+        self.model = model
+        self.memory = memory
+
+
+class Lawn_grass(Product):
+    def __init__(self, name, description, colour, price, quantity, country, germination_period):
+        super().__init__(name, description, colour, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+
+
 # код для проверки
 
-product_1 = Product('Iphone', 'an Apple phone', 599.50, 20)
-product_2 = Product('Ipod', 'an Apple media player', 249.50, 10)
-product_3 = Product('IMac', 'an Apple computer', 1999.50, 5)
-product_4 = Product('IMac', 'an Apple planchet', 449.50, 15)
+product_1 = Product('Iphone', 'an Apple phone', "Black", 599.50, 20)
+product_2 = Product('Ipod', 'an Apple media player', "Black", 249.50, 10)
+product_3 = Product('IMac', 'an Apple computer', "Black", 1999.50, 5)
+product_4 = Product('IMac', 'an Apple planchet', "Black", 449.50, 15)
 category_1 = Category("Apple devises", "Some devises from Apple",
                       [product_1, product_2, product_3, product_4])
 
-product_5 = Product('Phone', 'a phone', 99.50, 120)
-product_6 = Product('Player', 'a media player', 49.50, 210)
-product_7 = Product('PC', 'a computer', 1999.50, 5)
-product_8 = Product('planchet', 'a planchet', 99.50, 150)
+product_5 = Product('Phone', 'a phone', "Black", 99.50, 120)
+product_6 = Product('Player', 'a media player', "Black", 49.50, 210)
+product_7 = Product('PC', 'a computer', "Black", 1999.50, 5)
+product_8 = Product('planchet', 'a planchet', "Black", 99.50, 150)
 category_2 = Category("Devises", "Some devises from Apple",
                       [product_5, product_6, product_7, product_8])
 
 print(Category.category_num)
 print(Category.product_num)
 [print(x) for x in category_2.show_products_in_stock]
-product_11 = Product.new_product('кубик рубика', 'головоломка', 9.50, 10)
+product_11 = Product.new_product('кубик рубика', 'головоломка', "Black", 9.50, 10)
 print(product_11.price_)
 product_11.price_ = 3.88
 print(product_11)
